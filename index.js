46d82937ca6557dc0a36d69e5346dcac4adea0ac
mayuri vhargar <70538329+merry26@users.noreply.github.com>
2024-06-10 01:26:59 +0530

    Update index.js
    add front end link and cors

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
-const express= require('express')
-const mongoose = require('mongoose')
-const app=express()
-const routerAuth= require("./routes/auth")
-const routerNotes= require("./routes/notes")
-const cors= require('cors')
-require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
 
+// Load environment variables from .env file
+dotenv.config();
 
-app.use(express.json()) //middleware
-app.use("/auth",routerAuth)
-app.use("/notes", routerNotes)
-app.use(cors());
- 
-const PORT = process.env.PORT || 5000
+const app = express();
+const routerAuth = require("./routes/auth");
+const routerNotes = require("./routes/notes");
+
+// Middleware setup
+app.use(cors({
+    origin:["inotebook-frontend-omega.vercel.app"],
+    methods:["POST","GET","PUT","DELETE"],
+    credentials:true
+}));
+app.use(express.json()); // Body parser middleware
+
+// Routes setup
+app.use("/auth", routerAuth);
+app.use("/notes", routerNotes);
+
+const PORT = process.env.PORT || 5000;
 const BASE_URL = process.env.BASE_URL;
 const MONGO_URL = process.env.DATABASE;
 
-mongoose.connect(MONGO_URL).then(() => {
-    console.log("MongoDB database connected");
-    app.listen(PORT, () => {
-        console.log(`Express server is running at ${BASE_URL}`);
-    });
-}).catch((error) => console.error(error));
+// Connect to MongoDB
+mongoose.connect(MONGO_URL)
+    .then(() => {
+        console.log("MongoDB database connected");
+        // Start the server
+        app.listen(PORT, () => {
+            console.log(`Express server is running at ${BASE_URL}`);
+        });
+    })
+    .catch((error) => console.error(error));
